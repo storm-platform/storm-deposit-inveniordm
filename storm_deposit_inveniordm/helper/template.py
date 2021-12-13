d5d4b782309021b62d1350e1785f2e41c80e2733
Phelps <phelpsmemo@gmail.com>
2021-12-13 14:44:17 -0300

    change base render template

--- storm_deposit_inveniordm/helper/template.py
+++ storm_deposit_inveniordm/helper/template.py
@@ -7,22 +7,21 @@
 # details.
 
 import ast
-from jinja2 import Environment, PackageLoader
+from storm_commons.template import render_template as render_template_base
 
 
 def render_template(template_name: str, package_path: str, **template_objects):
     """Render a `storm-deposit-inveniordm` contrib template."""
-    env = Environment(
-        loader=PackageLoader("storm_deposit_inveniordm.contrib", package_path)
+
+    # rendering!
+    rendered_template = render_template_base(
+        template_name,
+        "storm_deposit_inveniordm.contrib",
+        package_path,
+        **template_objects
     )
 
-    # Metadata template file
-    template = env.get_template(template_name)
-
-    # Rendering the template and cleaning the output result.
-    rendered_template = template.render(**template_objects)
     rendered_template = rendered_template.replace("\n", "").replace("'", '"')
-
     return ast.literal_eval(rendered_template)
 
 
